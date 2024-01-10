69db3720d7880e4161c43768c635544b1513c72b
Jimmy39 <wanghanyu2000@126.com>
2024-01-10 22:26:03 +0800

    add write with memory

--- Llibrary/src/voile_iopin_74595.c
+++ Llibrary/src/voile_iopin_74595.c
@@ -1,5 +1,10 @@
 #include "voile_iopin_74595.h"
 
+#include "voile_uart_rp2040.h"
+
+extern voile_const_uart_t uart;
+
+
 voile_const_ioPin_Operate_t voile_const_ioPin_Operate_74595 = {
     .Init = (voile_status_t (*)(const void *, voile_io_mode_t, ...))voile_ioPin_Operate_Init_74595,
     .Write = (voile_status_t (*)(const void *, bool))voile_ioPin_Operate_Write_74595,
@@ -16,28 +21,57 @@ voile_const_ioPin_Get_t voile_const_ioPin_Get_74595 = {
 
 
 voile_status_t voile_ioPin_Operate_Init_74595(voile_const_internal_ioPin_74595_t *this, voile_io_mode_t mode, bool value) {
-    uint8_t i, date;
-    if(!voile_ioPin_Get_IfInit_74595(this)) {
+    int8_t i;
+    uint8_t date;
+    if ((this->chip->state == NULL)&&(this->chip->QH_ == NULL)) {
+        return failure;
+    }
+    if (!voile_ioPin_Get_IfInit_74595(this)) {
         voile_74595_Operate_Init(this->chip);
-        for(i = this->cascade; i > 0; i--) {
-            if((!value)&&(this->pinNumber/8 + 1 == i)) {
+        for (i = this->cascade - 1; i >= 0; i--) {
+            if ((!value)&&(this->pinNumber/8 == i)) {
                 date = 0xff^(1ul << (this->pinNumber%8));
-                voile_74595_Operate_ShiftBytes(this->chip, &date, 1);
             }
             else {
                 date = 0xff;
-                voile_74595_Operate_ShiftBytes(this->chip, &date, 1);
             }
+            voile_74595_Operate_ShiftBytes(this->chip, &date, 1);
+            if (this->chip->state != NULL) {
+                *(this->chip->state+i) = date;
+            }
+    uart.Operate->Transmit(&uart, i);
         }
         voile_74595_Operate_Load(this->chip);
-        voile_74595_Operate_Enable(this->chip);
+        if ((mode == IOmodeQuasiBidirectional)||(mode == IOmodePushPull)||(mode == IOmodeOpenDrain)) {
+            voile_74595_Operate_Enable(this->chip);
+        }
+    }
+    else {
+        voile_ioPin_Operate_Write_74595(this, value);
     }
     return success;
 }
 
 voile_status_t voile_ioPin_Operate_Write_74595(voile_const_internal_ioPin_74595_t *this, bool value) {
-    
-    return success;
+    if (this->chip->state != NULL) {
+        if (value) {
+            *(this->chip->state+this->pinNumber/8) |= 1ul << (this->pinNumber%8);
+        }
+        else {
+            *(this->chip->state+this->pinNumber/8) &= ~(1ul << (this->pinNumber%8));
+        }
+        voile_74595_Operate_ShiftBytes(this->chip, this->chip->state, this->cascade);
+    uart.Operate->Transmit(&uart, *(this->chip->state));
+        voile_74595_Operate_Load(this->chip);
+        return success;
+    }
+    else if (this->chip->QH_ != NULL) {
+        
+        return success;
+    }
+    else {
+        return failure;
+    }
 }
 
 voile_status_t voile_ioPin_Operate_Taggle_74595(voile_const_internal_ioPin_74595_t *ioPin_p) {
